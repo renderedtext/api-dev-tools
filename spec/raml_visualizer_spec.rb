11f97a3e54b608950a666d52df1e12049224cf49
Jovan Ivanović <jivanovic@renderedtext.com>
2017-05-30 15:37:32 +0200

    Update default spec

--- spec/raml_visualizer_spec.rb
+++ spec/raml_visualizer_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RamlVisualizer do
   it "has a version number" do
     expect(RamlVisualizer::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
