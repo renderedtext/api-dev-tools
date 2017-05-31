a7a6eaefcc4b6d6ff4fd73e917d9675e43e39ece
Jovan Ivanović <jivanovic@renderedtext.com>
2017-05-31 16:49:52 +0200

    Update root module

--- lib/raml_visualizer.rb
+++ lib/raml_visualizer.rb
@@ -1,6 +1,8 @@
+require "byebug"
 require "raml_visualizer/version"
 require "raml_visualizer/specification_json"
 require "raml_visualizer/resource"
+require "raml_visualizer/entity_page"
 require "raml_visualizer/root_controller"
 
 module RamlVisualizer
