53156779fd8a51482101f07f98ba071bff77095b
Jovan Ivanović <jivanovic@renderedtext.com>
2017-05-31 16:50:06 +0200

    Add an integration spec

---
+++ spec/generation_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+require "fileutils"
+
+RSpec.describe "Full integration" do
+  let(:input_file) { "spec/fixtures/json_output.json" }
+  let(:output_dir) { ".test_output" }
+  let(:controller) { RamlVisualizer::RootController.new(input_file, output_dir) }
+
+  describe "full integration" do
+    it "generates the entity pages" do
+      controller.generate_pages
+
+      content = File.open("#{output_dir}/users.html", "rb") { |file| file.read }
+
+      expect(content).to include("users")
+    end
+  end
+
+  after do
+    FileUtils.rm_rf(output_dir)
+  end
+end
