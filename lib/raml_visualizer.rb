d7084e79988c33164d63542974658d99c28856f5
Jovan Ivanović <jivanovic@renderedtext.com>
2017-06-01 15:56:28 +0200

    Remove byebug

--- lib/raml_visualizer.rb
+++ lib/raml_visualizer.rb
@@ -1,4 +1,3 @@
-require "byebug"
 require "raml_visualizer/version"
 require "raml_visualizer/specification_json"
 require "raml_visualizer/resource"
--- lib/raml_visualizer/version.rb
+++ lib/raml_visualizer/version.rb
@@ -1,3 +1,3 @@
 module RamlVisualizer
-  VERSION = "0.4.0"
+  VERSION = "0.5.0"
 end
