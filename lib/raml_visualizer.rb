4d87c252d6946e50e01b22b91ba62cfbc151699b
Jovan Ivanović <jivanovic@renderedtext.com>
2017-06-02 17:26:53 +0200

    Update root file

--- lib/raml_visualizer.rb
+++ lib/raml_visualizer.rb
@@ -1,8 +1,11 @@
 require "raml_visualizer/version"
 require "raml_visualizer/specification_json"
 require "raml_visualizer/resource"
+require "raml_visualizer/generator"
+require "raml_visualizer/html_generator"
+require "raml_visualizer/md_generator"
 require "raml_visualizer/page"
-require "raml_visualizer/page_factory"
+require "raml_visualizer/page_builder"
 require "raml_visualizer/root_controller"
 
 module RamlVisualizer
