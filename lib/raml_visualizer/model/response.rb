50a8a668b0b827a0cf3dea2b1bfa5437abae6906
Jovan Ivanović <jivanovic@renderedtext.com>
2017-06-05 13:12:12 +0200

    Minor fix

--- lib/raml_visualizer/model/response.rb
+++ lib/raml_visualizer/model/response.rb
@@ -1,8 +1,12 @@
 module RamlVisualizer
   module Model
     class Response < Element
+      def body
+        (@raw["body"] && @raw["body"].first).to_h
+      end
+
       def properties
-        raw_properties = @raw["body"].to_a.first.to_h["properties"].to_a
+        raw_properties = body["properties"].to_a
 
         @properties ||= raw_properties.map do |raw_property|
           Element.new(raw_property, self)
--- lib/raml_visualizer/version.rb
+++ lib/raml_visualizer/version.rb
@@ -1,3 +1,3 @@
 module RamlVisualizer
-  VERSION = "0.10.0"
+  VERSION = "0.10.1"
 end
