6e9ffb250cc27ac976a50ac5a318dc271d58e22c
Jovan Ivanović <jivanovic@renderedtext.com>
2017-06-05 14:17:47 +0200

    Enable rendering of tables and code blocks

--- lib/raml_visualizer/html_generator.rb
+++ lib/raml_visualizer/html_generator.rb
@@ -2,11 +2,16 @@ require "redcarpet"
 
 module RamlVisualizer
   class HtmlGenerator < Generator
+    RENDER_OPTIONS = {
+      :tables => true,
+      :fenced_code_blocks => true
+    }
+
     def initialize(destination_dir)
       @destination_dir = destination_dir
 
       html_render = Redcarpet::Render::HTML.new
-      @markdown = Redcarpet::Markdown.new(html_render)
+      @markdown = Redcarpet::Markdown.new(html_render, RENDER_OPTIONS)
     end
 
     def generate(name, raw_content)
