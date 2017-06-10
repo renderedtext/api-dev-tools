3bdc0d44f9a02320ebf7e92b4a0f310866af9354
Igor Šarčević <igor@renderedtext.com>
2017-06-11 00:51:02 +0200

    Markdown helper classes

--- raml_visualizer/lib/raml_visualizer/md_generator.rb
+++ raml_visualizer/lib/raml_visualizer/md_generator.rb
@@ -1,5 +1,39 @@
 module RamlVisualizer
   class MdGenerator < Generator
+
+    def self.request_table(route)
+      return "" if route.request.nil?
+
+      header = [
+        ["Name", "Type", "Required", "Example"],
+        ["----", "----", "--------", "-------"]
+      ]
+
+      data = route.request.properties.map do |f|
+        [f.name, f.type, f.required?, f.example]
+      end
+
+      rows = (header + data).map { |row| "| #{row.join(" | ")} |" }
+
+      "\n" + rows.join("\n") + "\n"
+    end
+
+    def self.response_example(route)
+      header = "HTTP status: #{route.succesfull_response.code}"
+
+      if route.succesfull_response.example
+        example = JSON.pretty_generate(route.succesfull_response.example)
+
+        fenced_code(header + "\n\n" + example)
+      else
+        fenced_code(header)
+      end
+    end
+
+    def self.fenced_code(content)
+      "\n\`\`\`\n#{content}\n\`\`\`\n"
+    end
+
     def generate(path, content)
       extended_path = "#{path}.md"
 
--- raml_visualizer/spec/raml_visualizer/md_generator_spec.rb
+++ raml_visualizer/spec/raml_visualizer/md_generator_spec.rb
@@ -11,4 +11,78 @@ RSpec.describe RamlVisualizer::MdGenerator do
       subject.generate(destination_path, content)
     end
   end
+
+  describe ".request_table" do
+    context "empty request" do
+      it "renders nothing" do
+        specs = RamlParser.load("spec/fixtures/json_output.json")
+
+        route = specs.find_route(:get, "/orgs/{username}")
+
+        expect(RamlVisualizer::MdGenerator.request_table(route)).to eq("")
+      end
+    end
+
+    context "non empty request" do
+      it "renders a markdown table" do
+        specs = RamlParser.load("spec/fixtures/json_output.json")
+
+        route = specs.find_route(:patch, "/teams/{id}")
+
+        table = RamlVisualizer::MdGenerator.request_table(route)
+
+        expect(table).to eq('''
+| Name | Type | Required | Example |
+| ---- | ---- | -------- | ------- |
+| name | string | false | developers |
+| permission | string | false | write |
+| description | string | false | Developers team |
+''')
+      end
+    end
+  end
+
+  describe ".response_example" do
+
+    context "response is not empty" do
+      it "renders only the example" do
+        specs = RamlParser.load("spec/fixtures/json_output.json")
+
+        route = specs.find_route(:get, "/orgs/{username}")
+
+        response_example = RamlVisualizer::MdGenerator.response_example(route)
+
+        expect(response_example).to eq('''
+```
+HTTP status: 200
+
+{
+  "id": "86e78b7e-2f9c-45a7-9939-ec2c9f6f64b5",
+  "name": "Rendered Text",
+  "url": "https://api.semaphoreci.com/v2/teams/86e78b7e-2f9c-45a7-9939-ec2c9f6f64b5",
+  "username": "renderedtext",
+  "created_at": "2017-06-10 16:59:51 +0200",
+  "updated_at": "2017-06-10 16:59:51 +0200"
+}
+```
+''')
+      end
+    end
+
+    context "response is empty" do
+      it "renders only the header" do
+        specs = RamlParser.load("spec/fixtures/json_output.json")
+
+        route = specs.find_route(:delete, "/teams/{id}")
+
+        response_example = RamlVisualizer::MdGenerator.response_example(route)
+
+        expect(response_example).to eq('''
+```
+HTTP status: 204
+```
+''')
+      end
+    end
+  end
 end
